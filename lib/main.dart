44d1d9d73f7b7f9b88450ad5f41f9367cc59c2d7
sang-luong-1504 <luongsang010s@gmail.com>
2021-08-10 04:59:59 +0700

    Hot fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_movie_app/screens/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]);
   runApp(MyApp());
 }
 
